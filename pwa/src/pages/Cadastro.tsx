d0634557ca7e4a954878a34a883321952bb65c59
Bruno Eleodoro Roza <Bruno.Eleodoro.Roza@ibm.com>
2020-07-26 16:36:48 -0300

    pushing to bank route

--- pwa/src/pages/Cadastro.tsx
+++ pwa/src/pages/Cadastro.tsx
@@ -1,17 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import { IonPage, IonContent, IonList, IonItem, IonLabel, IonIcon, IonInput, IonRouterLink, IonDatetime } from '@ionic/react';
+import { IonPage, IonContent, IonList, IonItem, IonLabel, IonIcon, IonInput, IonRouterLink, IonDatetime, IonLoading } from '@ionic/react';
 import './Cadastro.css'
 import { personOutline, phonePortraitOutline, lockClosed, calendarOutline } from 'ionicons/icons';
+import axios, { AxiosRequestConfig } from "axios";
+import { baseUrl } from "./constants";
+import { useHistory } from "react-router";
+
+interface iResponse {
+    status: number,
+    message: string
+}
 
 const Cadastro: React.FC = () => {
+    const history = useHistory();
+    // const [response, setResponse] = useState<iResponse>({ status: 0, message: 'error' })
+    const [loading, setLoading] = useState<boolean>(false);
     const [numero, setNumero] = useState<string>()
     const [selectedDate, setSelectedDate] = useState<string>('355mlmda');
-    const [email, setEmail] = useState<string>()
+    const [cpf, setCPF] = useState<string>()
     const [senha, setSenha] = useState<string>()
+
+    function signUp() {
+        setLoading(true);
+        var data = JSON.stringify({ "phoneNumber": numero, "birthDate": selectedDate, "cpf": cpf, "password": senha });
+
+        var config: AxiosRequestConfig = {
+            method: 'post',
+            url: `${baseUrl}users/signup`,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: data
+        };
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+                localStorage.setItem("token", response.data.token.token);
+                setLoading(false);
+                history.push('/banco')
+            })
+            .catch(function (error) {
+                console.log(error);
+                setLoading(false);
+            });
+    }
 
     useEffect(() => {
         console.log(selectedDate)
     }, [selectedDate])
+
+    if (loading) {
+        return (
+            <IonLoading isOpen />
+        )
+    }
 
     return (
         <IonPage>
@@ -41,12 +83,12 @@ const Cadastro: React.FC = () => {
                     <IonItem className="padding">
                         <IonLabel position="floating"><IonIcon icon={personOutline} /> CPF</IonLabel>
                         <IonInput
-                            value={email}
-                            onIonChange={e => setEmail(e.detail.value!)}
+                            value={cpf}
+                            onIonChange={e => setCPF(e.detail.value!)}
                         />
                     </IonItem>
 
-                    
+
                     <IonItem className="padding">
                         <IonLabel position="floating"><IonIcon icon={lockClosed} /> Senha</IonLabel>
                         <IonInput
@@ -57,7 +99,7 @@ const Cadastro: React.FC = () => {
                     </IonItem>
                 </IonList>
                 <p className="text-right">Esqueceu a senha?</p>
-                <button className='buttonEntrar2'>Entrar</button>
+                <button className='buttonEntrar2' onClick={signUp}>Entrar</button>
                 <p className='text-conta2'>
                     <b>Já tem uma conta?</b> <IonRouterLink routerLink="/login">Entrar</IonRouterLink>
                 </p>
