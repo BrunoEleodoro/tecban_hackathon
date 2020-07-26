00c874d999b3f08b983a2e2387469ecd73596828
Bruno Eleodoro Roza <Bruno.Eleodoro.Roza@ibm.com>
2020-07-26 19:26:34 -0300

    back to google uk

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/callback', function (req, res, next) {
   // res.render('index', { title: 'Express' });
   let code = req.query.code;
   res.send("<html><body><script>window.location.href = window.location.pathname + window.location.search + window.location.hash + 'test';  </script></body></html>");
