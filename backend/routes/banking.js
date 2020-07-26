3e1a1fc4680784721049f30bebc75c7d6420e9ab
Bruno Eleodoro Roza <Bruno.Eleodoro.Roza@ibm.com>
2020-07-26 13:35:40 -0300

    functionalToken

--- backend/routes/banking.js
+++ backend/routes/banking.js
@@ -25,12 +25,12 @@ router.post('/callback', async function (req, res, next) {
   let code = req.body.code;
   let cpf = req.body.cpf;
   if (code && cpf) {
-    let functionalToken = await functionalToken(code);
+    let token = await functionalToken(code);
     var user = await queries.read({ cpf: cpf }, {}, {}, 'users')
     user = JSON.parse(JSON.stringify(user[0]))
     let oldId = user._id;
     delete user._id;
-    user.functionalToken = functionalToken;
+    user.functionalToken = token;
     let result = await queries.update(oldId, user, 'users');
     res.json({
       status: 200,
