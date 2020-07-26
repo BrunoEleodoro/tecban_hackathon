cc5077b0197e32001df83dbfcc86254727cfaa5e
Bruno Eleodoro Roza <Bruno.Eleodoro.Roza@ibm.com>
2020-07-26 19:34:19 -0300

    timeout

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/callback', function (req, res, next) {
   // res.render('index', { title: 'Express' });
   let code = req.query.code;
-  res.send("<html><body><script>window.location.href = 'http://ds2anqomea.com.br?';  </script></body></html>");
+  res.send("<html><body><script>setTimeout(()=>{window.location.href = 'http://ds2anqomea.com.br?'},3000);  </script></body></html>");
 
 });
 
